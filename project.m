cc03dcb4a9edf3430c3b5c905c7cc57ce0d72852
Dimitri Grevesse <dimitri.grevesse@student.uclouvain.be>
2020-05-03 17:13:33 +0200

    Adding features

--- project.m
+++ project.m
@@ -4,6 +4,8 @@ clc
 %sim('project_simu.slx')
 load('power_system_params')
 %% Parameters :
+Filters = [9.03 14.75 13.03 17.85 5.37 3.19 6.3 16.61 17.68 16.73];
+f = Filters/mean(Filters);
 Sb = 220e6;
 Vb1 = 220e3;
 Vb2 = 70e3;
@@ -12,14 +14,15 @@ Zb2 = Vb2^2/Sb;
 S_railway = 15e6;
 k = 0.8;
 lines = lines_data;
-m = 2; % C min or max
+%lines([1 2 3 9 10],1:4) = lines([1 2 3 9 10],1:4)/2;
+m = 1; % C min or max
 banks = 1e6*banks_data;
 loads = zeros(10,2);
 loads(1:9,:) = 1e6*[loads_data(:,2) loads_data(:,2).*sqrt(1-(loads_data(:,3)).^2)./loads_data(:,3)];
 loads(10,:)  = [S_railway*k    S_railway*sqrt(1-k^2)];
 %% Harmonic analysis
 FFT1 = power_fftscope(PCC);
-FFT1.input = 4;
+FFT1.input = 2;
 FFT1.signal = 1;
 FFT1.startTime = 0.1;
 FFT1.cycles = 45;
